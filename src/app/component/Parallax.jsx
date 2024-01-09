4271772239d7eb238f819359ba805af915371ae8
patrykr124 <patrykr124@gmail.com>
2024-01-09 21:33:59 +0100

    09 image1

--- src/app/component/Parallax.jsx
+++ src/app/component/Parallax.jsx
@@ -7,7 +7,18 @@ import "./Parallax.css";
 function Parallax() {
   return (
     <div className="parallax_section w-full">
-      <div className="parallax_image flex items-center justify-center">
+      <div className="parallax_image flex items-center justify-center"
+        style={{
+          backgroundImage: `url('image/carouzel5.jpg')`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "100%",
+          
+        }}
+        aria-label="Background Header cloud">
       <div className="button_box">
       <button className="button_hero font-bold overflow-hidden relative px-8 py-3 bg-transparent border-2 mt-10" >
         <ScrollLink  to="form">
